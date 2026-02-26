92bc9789302f810bfc6cdb56d75686e3ed04bfef
L Carver <wifihum@gmail.com>
2026-02-26 12:22:15 -0800

    Simplified Die::roll().

--- source/Die.cpp
+++ source/Die.cpp
@@ -13,13 +13,13 @@
 // constants
 //------------------------------------------------------------------------------
 constexpr int DEFAULT_SIDES = 6;
-constexpr int MIN_ROLL = 1;
 
 //------------------------------------------------------------------------------
 // default constructor
 // - calls overload constructor with default number of sides for die
 //------------------------------------------------------------------------------
-Die::Die() : Die(DEFAULT_SIDES) {}
+Die::Die() : sides(DEFAULT_SIDES)
+{ }
 
 //------------------------------------------------------------------------------
 // overload constructor 
@@ -29,13 +29,9 @@ Die::Die() : Die(DEFAULT_SIDES) {}
 Die::Die(int numSides) : sides(numSides) { roll(); }
 
 //------------------------------------------------------------------------------
-// - simulates a die roll with random value from MIN_ROLL to number of sides
+// simulates a die roll with random value from 1 to number of sides
 //------------------------------------------------------------------------------
-int Die::roll()
-{
-	value = (rand() % (sides - MIN_ROLL + 1)) + MIN_ROLL;
-	return value;
-}
+int Die::roll() { return rand() % sides + 1; }
 
 //------------------------------------------------------------------------------
 // getters
